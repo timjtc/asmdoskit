f06097d34aa1f687c61859e3e48e7643aba24c21
Timothy Concepcion <timothyjtc@outlook.com>
2024-02-11 14:47:25 +0800

    merged functionality of mntlink.bat to start.bat

--- start.bat
+++ start.bat
@@ -1,8 +1,12 @@
 @echo off
 
-if not exist mnt_link goto nolink
+if exist mnt_link rmdir mnt_link
+set /p "dirlink=Enter path to mount: "
+if [%dirlink%] == [] goto nolink
 
-:main
+:makelink
+mklink /J mnt_link %dirlink%
+if errorlevel 1 goto err1
 cd dosbox
 start /W DOSBox.exe
 cd ..
@@ -10,11 +14,15 @@ rmdir mnt_link
 goto end
 
 :nolink
-echo No link found. Please run mntlink.bat to create a working directory link.
+echo No input given. Please run again and provide a directory to link if you want a custom working directory to be mounted.
 echo For now, the empty directory mnt_d will be used as the working directory.
 pause
-echo.
-mklink /J mnt_link mnt_d
-goto main
+set dirlink=mnt_d
+goto makelink
+
+:err1
+echo Error creating link. Please check the directory path and try again.
+pause
+goto end
 
 :end
